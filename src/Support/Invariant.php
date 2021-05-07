64ed8d04c48384470131e753777ae5c16bbe7c23
Reid Andrew Barden <me@reidbarden.com>
2021-05-06 21:42:40 -0400

    adds order and size

--- src/Support/Invariant.php
+++ src/Support/Invariant.php
@@ -8,6 +8,24 @@ use Rbarden\Graph\Base\Graph;
 
 final class Invariant
 {
+    public static function order(Graph $graph): int
+    {
+        $count = count(array_keys($graph->getAdjacencyList()));
+
+        return $count;
+    }
+
+    public static function size(Graph $graph): int
+    {
+        $count = array_sum(array_map(static fn ($values) => count($values), $graph->getAdjacencyList()));
+
+        if (! $graph->isDirected) {
+            $count /= 2;
+        }
+
+        return $count;
+    }
+
     public static function getOutDegrees(Graph $graph): array
     {
         return array_map(
--- tests/Support/InvariantTest.php
+++ tests/Support/InvariantTest.php
@@ -11,6 +11,7 @@ use Rbarden\Graph\Support\Invariant;
 class InvariantTest extends TestCase
 {
     private Graph $graph;
+    private Graph $graph2;
 
     protected function setUp(): void
     {
@@ -20,10 +21,33 @@ class InvariantTest extends TestCase
             2 => [3],
             3 => [],
         ]);
+
+        $this->graph2 = new Graph(false);
+        $this->graph2->setAdjacencyList([
+            1 => [2, 3],
+            2 => [1, 3],
+            3 => [1, 2],
+        ]);
     }
 
     public function testOutDegreesAreCalculatedCorrectly(): void
     {
         self::assertEquals([1 => 2, 2 => 1, 3 => 0], Invariant::getOutDegrees($this->graph));
+    }
+
+    public function testOrderIsCalculatedCorrectly(): void
+    {
+        self::assertEquals(3, Invariant::order($this->graph));
+        self::assertEquals(0, Invariant::order(new Graph()));
+
+        self::assertEquals(3, Invariant::order($this->graph2));
+    }
+
+    public function testSizeIsCalculatedCorrectly(): void
+    {
+        self::assertEquals(3, Invariant::size($this->graph));
+        self::assertEquals(0, Invariant::size(new Graph()));
+
+        self::assertEquals(3, Invariant::size($this->graph2));
     }
 }
